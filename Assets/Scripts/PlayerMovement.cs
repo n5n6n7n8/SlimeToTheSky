705cb8a0fb52ac3af9399ddde2261c5781f291fc
Nate Wong <nwwong17@gmail.com>
2026-05-10 12:13:26 -0700

    testing script

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,6 +5,7 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float jumpAmt = 5f;
+    [SerializeField] private float moveSpeed = 2f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     public Rigidbody rb;
     private bool shouldJump = false;
@@ -25,6 +26,14 @@ public class PlayerMovement : MonoBehaviour
         if (keyboard.spaceKey.wasPressedThisFrame)
         {
             shouldJump = true;
+        }
+        if (keyboard.aKey.isPressed)
+        {
+            rb.AddForce(Vector3.left * moveSpeed, ForceMode.Force);
+        }
+        if (keyboard.dKey.isPressed)
+        {
+            rb.AddForce(Vector3.right * moveSpeed, ForceMode.Force);
         }
     }
     void FixedUpdate()
